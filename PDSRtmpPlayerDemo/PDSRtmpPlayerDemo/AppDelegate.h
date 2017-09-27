954a706bc5a140e95418820d18f457df9ffa44c4
w91379137 <yc@spe3d.co>
2017-09-28 00:00:29 +0800

    add project

---
+++ PDSRtmpPlayerDemo/PDSRtmpPlayerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PDSRtmpPlayerDemo
+//
+//  Created by w91379137 on 2017/9/27.
+//  Copyright © 2017年 w91379137. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PDSRtmpPlayerDemo/PDSRtmpPlayerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PDSRtmpPlayerDemo
+//
+//  Created by w91379137 on 2017/9/27.
+//  Copyright © 2017年 w91379137. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
