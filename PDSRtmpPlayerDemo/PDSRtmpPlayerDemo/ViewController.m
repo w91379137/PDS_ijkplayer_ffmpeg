483ef4e227d072ecf1f44dba40129e0f4ef3589d
林永璋 <yclin@jumbogames.com.tw>
2017-12-06 16:47:12 +0800

    按照演練流程 走一遍

--- PDSRtmpPlayerDemo/PDSRtmpPlayerDemo/ViewController.m
+++ PDSRtmpPlayerDemo/PDSRtmpPlayerDemo/ViewController.m
@@ -13,8 +13,15 @@
 @interface ViewController ()
 
 @property(nonatomic, strong) IJKFFMoviePlayerController *playerVC;
+
 @property(nonatomic, strong) IBOutlet UIView *baseView;
 @property(nonatomic, strong) WKWebView *webView;
+@property(nonatomic, strong) IBOutlet UIButton *flowControlButton;
+
+@property(nonatomic, strong) NSArray *videoURLArray;
+@property(nonatomic) int videoIndex;
+
+@property(nonatomic) int status;
 
 @end
 
@@ -23,50 +30,9 @@
 -(void)viewDidLoad {
     [super viewDidLoad];
     
-    WKWebViewConfiguration *webViewConfig = [[WKWebViewConfiguration alloc] init];
-    self.webView = [[WKWebView alloc] initWithFrame: self.view.frame configuration:webViewConfig];
-    
-    self.webView.opaque = NO;
-    self.webView.backgroundColor = [UIColor clearColor];
-    self.webView.scrollView.backgroundColor = [UIColor clearColor];
-    
-    [self.view addSubview:self.webView];
-    self.webView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-}
-
--(void)viewDidAppear:(BOOL)animated {
-    [super viewDidAppear:animated];
-    [self loadVideo];
-    [self loadHtml];
-}
-
-- (void)loadVideo {
-    //NSString *urlPath = @"http://live.hkstv.hk.lxdns.com/live/hks/playlist.m3u8";
-    
     //http://blog.csdn.net/chinabinlang/article/details/45092297
-    NSString *urlPath = @"rtmp://live.hkstv.hk.lxdns.com/live/hks"; //香港卫视
-    //NSString *urlPath = @"rtmp://v1.one-tv.com/live/mpegts.stream"; //亚太第一卫视
-    [self rtmpPlay:urlPath];
-    
-    
-    //測試影片播放時 可以 正常縮放
-    [UIView animateWithDuration:5
-                          delay:0
-                        options:UIViewKeyframeAnimationOptionAutoreverse | UIViewKeyframeAnimationOptionRepeat
-                     animations:^{
-                         self.baseView.transform = CGAffineTransformMakeScale(0.5, 0.5);
-                     }
-                     completion:nil];
-    
-    //要測試換網址 stop 重新 開始會怎樣
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        
-        [self.playerVC stop];
-        [self.playerVC.view removeFromSuperview];
-        
-        NSString *urlPath = @"rtmp://v1.one-tv.com/live/mpegts.stream";
-        [self rtmpPlay:urlPath];
-    });
+    self.videoURLArray = @[@"rtmp://live.hkstv.hk.lxdns.com/live/hks",
+                           @"rtmp://v1.one-tv.com/live/mpegts.stream"];
 }
     
 - (void)rtmpPlay:(NSString *)urlPath {
@@ -77,16 +43,106 @@
     [self.baseView addSubview:self.playerVC.view];
     self.playerVC.view.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
     
-    dispatch_async(dispatch_get_main_queue(), ^{
-        [self.playerVC prepareToPlay];
-        [self.playerVC play];
-    });
+    [IJKFFMoviePlayerController setLogLevel:k_IJK_LOG_SILENT];
+    
+    [self.playerVC prepareToPlay];
+    [self.playerVC play];
 }
 
-- (void)loadHtml {
-    NSURL *url = [[NSBundle mainBundle] URLForResource:@"index" withExtension: @"html"];
-    NSURLRequest *request = [NSURLRequest requestWithURL: url];
-    [self.webView loadRequest: request];
+- (IBAction)tapButton:(UIButton *)sender {
+    
+    NSString *title = @"";
+    switch (self.status) {
+        case 0: {
+            title = @"載入網頁";
+            
+            WKWebViewConfiguration *webViewConfig = [[WKWebViewConfiguration alloc] init];
+            self.webView = [[WKWebView alloc] initWithFrame: self.view.frame configuration:webViewConfig];
+            
+            self.webView.opaque = NO;
+            self.webView.backgroundColor = [UIColor clearColor];
+            self.webView.scrollView.backgroundColor = [UIColor clearColor];
+            
+            [self.view insertSubview:self.webView belowSubview:self.flowControlButton];
+            self.webView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+            
+            NSURL *url = [[NSBundle mainBundle] URLForResource:@"index" withExtension: @"html"];
+            NSURLRequest *request = [NSURLRequest requestWithURL: url];
+            [self.webView loadRequest: request];
+        }
+            break;
+        case 1: {
+            title = @"is enable player";
+        }
+            break;
+        case 2: {
+            title = @"get video URL";
+        }
+            break;
+        case 3: {
+            title = @"player frame";
+            //測試影片播放時 可以 正常縮放
+            //UIViewKeyframeAnimationOptionAutoreverse | UIViewKeyframeAnimationOptionRepeat
+            [UIView animateWithDuration:0.25
+                                  delay:0
+                                options:UIViewAnimationOptionBeginFromCurrentState
+                             animations:^{
+                                 self.baseView.transform = CGAffineTransformMakeScale(0.5, 0.5);
+                             }
+                             completion:nil];
+        }
+            break;
+        case 4: {
+            title = @"play";
+            [self.playerVC stop];
+            [self.playerVC.view removeFromSuperview];
+            [self rtmpPlay:self.videoURLArray[self.videoIndex]];
+        }
+            break;
+        case 5: {
+            title = @"stop";
+            [self.playerVC pause];
+        }
+            break;
+        case 6: {
+            title = @"player orientation";
+            [UIView animateWithDuration:0.25
+                                  delay:0
+                                options:UIViewAnimationOptionBeginFromCurrentState
+                             animations:^{
+                                 self.baseView.transform = CGAffineTransformMakeScale(1, 1);
+                             }
+                             completion:nil];
+        }
+            break;
+        case 7: {
+            title = @"event call back";
+        }
+            break;
+            
+        default:
+            title = @"重新";
+            [self.webView loadRequest: [NSURLRequest requestWithURL:[NSURL URLWithString:@"about:blank"]]];
+            self.videoIndex += 1;
+            self.videoIndex %= self.videoURLArray.count;
+            self.status = -1;
+            break;
+    }
+    
+    [self.flowControlButton setTitle:title forState:UIControlStateNormal];
+    self.status += 1;
+    //NSLog(@"%d", self.status);
 }
 
 @end
+
+/*
+ WebPlayerProtocol
+ 1. is enable player
+ 2. get video URL
+ 3. player frame
+ 4. play
+ 5. stop
+ 6. player orientation
+ 7. event call back
+ */
